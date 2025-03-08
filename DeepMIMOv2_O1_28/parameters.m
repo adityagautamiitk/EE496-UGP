b9f4118de90084e95938bdef279a785fac9ae501
adityagautamiitk <144600870+adityagautamiitk@users.noreply.github.com>
2025-03-08 15:44:39 +0530

    updated parameters

--- DeepMIMOv2_O1_28/parameters.m
+++ DeepMIMOv2_O1_28/parameters.m
@@ -2,18 +2,18 @@
 % A detailed description of the parameters is available on DeepMIMO.net
 
 %Ray-tracing scenario
-params.scenario = 'O1_60';          % The adopted ray tracing scenario [check the available scenarios at https://deepmimo.net/scenarios/]
+params.scenario = 'O1_28';          % The adopted ray tracing scenario [check the available scenarios at https://deepmimo.net/scenarios/]
 
 %Dynamic Scenario Scenes [only for dynamic (multiple-scene) scenarios]
 params.scene_first = 1;
 params.scene_last = 1;
 
 % Active base stations
-params.active_BS = [1];             % Includes the numbers of the active BSs (values from 1-18 for 'O1')(check the scenario description at https://deepmimo.net/scenarios/ for the BS numbers) 
+params.active_BS = [3];             % Includes the numbers of the active BSs (values from 1-18 for 'O1')(check the scenario description at https://deepmimo.net/scenarios/ for the BS numbers) 
 
 % Active users
-params.active_user_first = 1;       % The first row of the considered user section (check the scenario description for the user row map)
-params.active_user_last = 1;        % The last row of the considered user section (check the scenario description for the user row map)
+params.active_user_first = 700;       % The first row of the considered user section (check the scenario description for the user row map)
+params.active_user_last = 1300;        % The last row of the considered user section (check the scenario description for the user row map)
 
 % Subsampling of active users
 %--> Setting both subsampling parameters to 1 activate all the users indicated previously
@@ -21,14 +21,14 @@ params.row_subsampling = 1;         % Randomly select round(row_subsampling*(act
 params.user_subsampling = 1;        % Randomly select round(user_subsampling*number_of_users_in_row) users in each row
 
 % Antenna array dimensions
-params.num_ant_BS = [1, 8, 4];      % Number of antenna elements for the BS arrays in the x,y,z-axes
+params.num_ant_BS = [1, 64, 1];      % Number of antenna elements for the BS arrays in the x,y,z-axes
 % By defauly, all BSs will have the same array sizes
 % To define different array sizes for the selected active BSs, you can add multiple rows. 
 % Example: For two active BSs with a 8x4 y-z UPA in the first BS and 4x4
 % x-z UPA for the second BS, you write  
 % params.num_ant_BS = [[1, 8, 4]; [1, 4, 4]];
 
-params.num_ant_UE = [1, 4, 2];      % Number of antenna elements for the user arrays in the x,y,z-axes
+params.num_ant_UE = [1, 1, 1];      % Number of antenna elements for the user arrays in the x,y,z-axes
 
 % Antenna array orientations
 params.activate_array_rotation = 0; % 0 -> no array rotation - 1 -> apply the array rotation defined in params.array_rotation_BS and params.array_rotation_UE
@@ -58,7 +58,7 @@ params.radiation_pattern = 0;         % 0: Isotropic and
                                       % 1: Half-wave dipole
                                     
 % System parameters
-params.bandwidth = 0.05;              % The bandwidth in GHz
+params.bandwidth = 0.5;              % The bandwidth in GHz
 params.activate_RX_filter = 0;        % 0 No RX filter 
                                       % 1 Apply RX low-pass filter (ideal: Sinc in the time domain)
 
@@ -70,6 +70,6 @@ params.num_paths = 5;                 % Maximum number of paths to be considered
 % OFDM parameters
 params.num_OFDM = 512;                % Number of OFDM subcarriers
 params.OFDM_sampling_factor = 1;      % The constructed channels will be calculated only at the sampled subcarriers (to reduce the size of the dataset)
-params.OFDM_limit = 64;               % Only the first params.OFDM_limit subcarriers will be considered  
+params.OFDM_limit = 32;               % Only the first params.OFDM_limit subcarriers will be considered  
 
 params.saveDataset = 0;               % 0: Will return the dataset without saving it (highly recommended!) 
